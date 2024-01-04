907e509ca53c06d74b4a2bc545c2edf44f796384
rishikasrinivas <04rishikasrinivas@gmail.com>
2024-01-03 21:46:18 -0800

    weather api

---
+++ lib/backend/store_searches.py
@@ -0,0 +1,22 @@
+import firebase_admin 
+from firebase_admin import credentials
+from firebase_admin import firestore
+import weather_api 
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("/Users/rishikasrinivas/Documents/Rishika/UCSC/Projects/Weather-App-UI-Flutter/weatherapp-6d528-firebase-adminsdk-4adbr-bab2f67e6d.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+city = "Cupertino"
+w_data, min=weather_api.get_weather_data(city)
+data = {
+    'data':w_data,
+    'min': min
+}
+
+entry_doc = db.collection('tasks').document()
+entry_doc.set(data)
+
+print(entry_doc.id)
